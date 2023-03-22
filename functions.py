81bc9327b74d1bf2e0bc84c4875ba9741ed2d614
Leopoldo <rokohaster@gmail.com>
2023-03-22 11:12:20 +0100

    commit_4

--- functions.py
+++ functions.py
@@ -3,13 +3,34 @@ def read_data(fichero):
  with open(fichero, 'r') as file:
     reader = csv.reader(file)
     dic = {
-            1 : {'type': white, 
+            1 : {'type': 'white', 
             'fixed acidity': '7', 
+            'volatile acidity':'0.27',
+            'citric acid' : '0.36',
+            'residual sugar':'20.7',
+            'chlorides':'0.045',
+            'free sulfur dioxide' : '45',
+            'total sulfur dioxide':'170',
+            'PH': '3',
+            'sulphates': '0.45',
+            'alcohol':'8.8',
+            'quality':'6'
             }
     }
     suma = 1
     for row in reader:
-        dic.update({suma:{'type' = row[0],'fixed acidity' = row[1]}})
-        print(row)
-        suma = suma+1
-read_data('winequality.csv')
+      if (len(reader)<10):
+        print("ha habido un error")
+        
+
+      
+      dic1 = dic[suma]
+      dic1.update({'type' : row[0],'fixed acidity':row[1],'volatile acidity':row[2]})
+      dic1.update({'residual sugar':row[3],'chlorides':row[4],'free sulfur dioxide':row[5]})
+      dic1.update({'total sulfur dioxide':row[6],'PH':row[5],'sulphates':row[6]})
+      dic1.update({'sulphates':row[7],'alcohol':row[8],'quality':row[9]})
+      suma = suma+1
+      dic.update({suma:dic1})       
+      
+    return dic 
+print(read_data('winequality.csv'))
