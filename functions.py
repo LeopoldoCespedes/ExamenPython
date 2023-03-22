95ce5a9da0b2ee1b5bf78ff70ee534ecbcdd215b
Leopoldo <rokohaster@gmail.com>
2023-03-22 10:45:47 +0100

    commit_3

--- functions.py
+++ functions.py
@@ -2,8 +2,14 @@ import csv
 def read_data(fichero):
  with open(fichero, 'r') as file:
     reader = csv.reader(file)
-    dic = {1:{}}
+    dic = {
+            1 : {'type': white, 
+            'fixed acidity': '7', 
+            }
+    }
+    suma = 1
     for row in reader:
-        
+        dic.update({suma:{'type' = row[0],'fixed acidity' = row[1]}})
         print(row)
+        suma = suma+1
 read_data('winequality.csv')
